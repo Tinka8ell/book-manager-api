a3d71c89b63c53ca0064aa626910ff13b34fba93
Mark Ingamells <mark.ingamells@gmail.com>
2022-05-09 12:54:51 +0100

    Small fix in Book model thrown up by InteliJ

--- src/main/java/com/techreturners/bookmanager/model/Book.java
+++ src/main/java/com/techreturners/bookmanager/model/Book.java
@@ -1,17 +1,19 @@
 package com.techreturners.bookmanager.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.Builder;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -33,4 +35,17 @@ public class Book {
 
     @Column
     Genre genre;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Book book = (Book) o;
+        return id != null && Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
